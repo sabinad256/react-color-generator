b6dbb2285f13d3c64525a6d05f7d9a0b68c5f737
sabinad256 <sabinad256@gmail.com>
2019-04-20 22:40:17 +0800

    Initial code for RGB color generator.

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+        this.updateCanvas();
+    }
+    updateCanvas() {
+
+        const ctx = this.refs.canvas.getContext('2d'); //initilization of canvas
+   
+        for(let i=0; i<32; i++){
+          var x = 0; // x cordinates
+          var y = 0; // y cordinates
+          var size = 10; // Size of the box for color. Change this to increase size of the box
+          for(let j = 0; j<32; j++){
+            for(let k=0; k<32; k++){
+              //fill color to box
+              ctx.fillStyle = `rgb(
+                ${Math.floor(8 * k)},
+                ${Math.floor(8 * j)},
+                ${Math.floor(8 * i)})`;
+                   x = k+(i%8)*32;
+                   y = j+parseInt(i/8)*32;
+                  
+                   ctx.fillRect(x*size,y*size,size,size); //draw rectangular box
+                
+            }
+          }
+            
+        }
+
+        
+    }
+    render() {
+        return (
+            <canvas ref="canvas" width={2560} height={1280}/>
+        );
+    }
+}
+
+export default App;
